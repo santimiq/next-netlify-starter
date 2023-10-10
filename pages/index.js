f15489b210d7b11cde67b943b16e80eca2fdd04e
Santiago Miqueo <46029931+santimiq@users.noreply.github.com>
2023-10-10 12:24:06 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Delta Protect!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Subdomain Takeover Hexa.dd360.mx <code>pages/index.js</code>
         </p>
       </main>
 
